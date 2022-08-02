dda003ddecb8ec3c75221fc33f7f8dc4e3de53e2
tektechnologies <info@tektechnologies.com>
2022-08-01 23:38:35 -0500

    add routes and Pizzaconstr

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 'use strict';
-console.log('server.js connected.');
+console.log('!!server.js connected.');
 
 
 // REQUIRE
@@ -11,6 +11,7 @@ console.log('server.js connected.');
 const express = require('express');
 //how we bring in the env file
 require('dotenv').config();
+let data = require('./data/pizza.json');
 
 
 
@@ -35,6 +36,78 @@ const PORT = process.env.PORT || 3002;
 
 
 //Routes we will use to access our end point
+/**
+ * .get() is an express method
+ * it correlates to axios.get
+ * the first parameter is a URL in quote
+ * the second is a callback function
+ */
+// our root of our site pass callback () two params
+app.get('/', (request, response) => {
+  //then we need to send something back.
+  response.send('hello from our server!!');
+});
+
+
+app.get('/hello', (request, response) => {
+  // localhost:3001/hello?name=bob
+  // query: { name: 'bob' }, and the URL in the req
+  console.log('request object: ', request.query.name);
+  let name = request.query.name;
+  let lastName = request.query.lastname;
+  // response.send('Hello from our /hello route!');
+  response.send(`Hello ${name} ${lastName}`);
+});
+
+//add a pizza route
+app.get('/pizza', (request, response) => {
+  let pizzaType = request.query.pizzatype;
+  // http://localhost:3001/pizza?pizzatype=Chicago%20Pizza
+  //add data file and look at find(will find the first and returns only that match)
+  // let dataToSend = data.find(pizza => pizza.pizzatype === pizzaType);
+  let dataToInstant = data.find(pizza => pizza.pizzatype === pizzaType);
+  let dataToSend = new Pizza(dataToInstant);
+  response.send(dataToSend);
+  // now create a class below
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+app.get('*', (request, response) => {
+  response.send('The route was not found. Error 404');
+});
+
+
+
+
+//Classes
+//so we can send over an object and it will construct it as an instance of our pizza object
+class Pizza{
+  constructor(pizzaObject){
+    console.log('Pizza instance construction: ',pizzaObject);
+    this.pizzaType = pizzaObject.pizzatype;
+    this.location = pizzaObject.location;
+  }
+}
+
+
 
 
 
